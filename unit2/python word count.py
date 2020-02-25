00c922fa1f77340590983808d4d4d2a3b790c68e
wnz <wnz_99@hust.edu.cn>
2020-02-25 11:41:12 +0800

    word count upgrade2

--- unit2/python word count.py
+++ unit2/python word count.py
@@ -1,19 +1,30 @@
 # coding=utf-8
 
+
+def word_count(filename):
+    import os.path
+    if os.path.isfile(filename):
+        with open(filename, 'r') as file:
+            text = file.read()
+            char_list = [
+                ',', '.', ';', ':', '(', ')', '!', '?', '\n', '\t', ' \"',
+                ' \'', '/', '\\'
+            ]
+            # 单词间分割符列表
+
+            for each_char in char_list:
+                text = text.replace(each_char, ' ')
+            # 将char_list中的符号全部替换为空格
+
+            words = text.split()
+            # 以空格为界，分割拆分文本。保存为list
+            words_num = len(words)
+
+    return words_num
+
+
 file = './unit2/readme.md'
 
-text = open(file).read()
-# 读取文件
+word_num = word_count(file)
 
-char_list = [',', '.', '!', '?', '\n', '\t', '\"', '\'']
-# 单词间分割符列表
-
-for each_char in char_list:
-    text = text.replace(each_char, ' ')
-# 将char_list中的符号全部替换为空格
-
-words = text.split()
-
-# 以空格为界，分割拆分文本。保存为list
-
-print('Word count:\t', len(words))
+print('word numbers', word_num)
